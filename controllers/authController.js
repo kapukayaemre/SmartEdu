c6e9efcf405bb8602ec8d44f2f0dcf41dfb00d66
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-02-28 19:53:08 +0300

    Login system integrated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -7,6 +8,24 @@ exports.createUser = async (req, res) => {
       status: 'success',
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    let same = await bcrypt.compare(password, user.password);
+    if (same) {
+      res.status(200).send('You are logged in');
+    } else {
+      res.send('Geçersiz');
+    }
   } catch (error) {
     res.status(400).json({
       status: 'fail',
