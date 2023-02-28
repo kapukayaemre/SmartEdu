ca76b105d3b44ea865ee6da071e8752e66f2ce78
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-02-28 12:48:22 +0300

    Course controller, route and model created

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
-const pageRoute = require('./routes/pageRoute'); 
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+
 const app = express();
+
+//! Connect DB
+mongoose
+  .connect('mongodb://127.0.0.1:27017/smartedu-db')
+  .then(() => console.log('Connected!'));
 
 //! Template Engine
 app.set('view engine', 'ejs');
 
 //! Middlewares
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //! Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
-app.get('/about', );
+app.get('/about');
 
 const port = 3000;
 app.listen(port, () => {
